e908c2558a7a302c2980893535035fb80eb7841c
omer <omerbar450@gmail.com>
2022-11-16 16:08:53 +0200

    commiting the distance header and cpp file with the euclidean algorithm

---
+++ calculate/Distance.h
@@ -0,0 +1,46 @@
+//
+// Created by omerb on 16/11/2022.
+//
+
+#ifndef ADVANCED_PROGRAMMING_DISTANCE_H
+#define ADVANCED_PROGRAMMING_DISTANCE_H
+#include <vector>
+#include <type_traits>
+#include <cmath>
+
+class Distance {
+protected:
+
+    /**
+     * check if two giving vector can be added/multi. check if the size is the same.
+     * @param v1 - vector type T generic
+     * @param v2 - vector type T generic
+     * @return
+     */
+    bool checkVectors(std::vector<double> v1, std::vector<double> v2);
+
+    /**
+     * this function takes two vectors and for each element in the vector it calculate |x1 - x2|
+     * and assign it to the result vector.
+     * @param v1
+     * @param v2
+     * @return
+     */
+    std::vector<double> absVector(std::vector<double> v1, std::vector<double> v2);
+
+public:
+
+    /**
+     * for each algorithm that going to inheritance the distance class, need's to implements the
+     * calculation algorithm.
+     * @param v1 - vector type T generic
+     * @param v2 - vector type F generic
+     * @return return the distance between them.
+     */
+    virtual double operator()(std::vector<double> v1, std::vector<double> v2) = 0;
+
+};
+
+
+
+#endif //ADVANCED_PROGRAMMING_DISTANCE_H
---
+++ main.cpp
@@ -0,0 +1,37 @@
+//
+// Created by shilo on 12/11/2022.
+//
+//#include "calculation//Distance.h"
+#include <iostream>
+#include <string>
+#include "calculate/Distance.h"
+#include "calculate/Euclidean.h"
+
+using namespace std;
+
+int main() {
+    std::string vec1;
+    std::string vec2;
+    string input;
+
+    //std::cout << "Enter vectors: " << std::endl;
+    //getline(cin, vec1);
+    //getline(cin, vec2);
+
+    //Distance :: checkPropriety("34 23 12" , "9 23 43");
+
+    std::vector<double> v2 = {1, 2, 3};
+    std::vector<double> v1 = {3, 2, 1};
+//    getline(cin, vec2);
+//    double b2 = Distance::manhattan(v1, v2);
+//    double b3 = Distance::canberra(v1, v2);
+//    std::cout.precision(17);
+//    std::cout << b3 ;
+
+    Euclidean calculate;
+    Distance* d = &calculate;
+
+    cout << calculate(v1, v2) << endl;
+
+    return 0;
+}
