ef9b826ca2259cc58454cb6282fa1f4ff02dac64
omer <omerbar450@gmail.com>
2022-12-03 19:49:40 +0200

    main update

--- calculate/Algorithim/CSVStruct.h
+++ calculate/Algorithim/CSVStruct.h
@@ -6,13 +6,20 @@
 #define RUNNABLE_CSVSTRUCT_H
 
 /**
- * creating struct to hold the distance data.
+ * DistanceVec struct
+ * double vData - store the distance between some vector v1 to some inputVector.
+ * std::string hold the vector classified name tag.
  */
 struct DistanceVec{
     double vData;
     std::string className;
 };
 
+/**
+ * VectorCSV struct
+ * std::vector<double> hold the vector value.
+ * std::string hold the classified name of the vector tag.
+ */
 struct VectorCSV {
     std::vector<double> id;
     std::string name;
