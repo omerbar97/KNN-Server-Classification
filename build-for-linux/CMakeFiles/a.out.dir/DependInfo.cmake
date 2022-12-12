d542ebfc8792e7cac7fe8bf40e1ecc2707b8806c
omer <omerbar450@gmail.com>
2022-12-12 22:22:32 +0200

    adding build files

---
+++ build-for-linux/CMakeFiles/a.out.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/omer/Desktop/projects in linux/advenced programming/Advanced-Programming-Project/calculate/Algorithim/Knn.cpp" "CMakeFiles/a.out.dir/calculate/Algorithim/Knn.cpp.o" "gcc" "CMakeFiles/a.out.dir/calculate/Algorithim/Knn.cpp.o.d"
+  "/home/omer/Desktop/projects in linux/advenced programming/Advanced-Programming-Project/calculate/Algorithim/Select.cpp" "CMakeFiles/a.out.dir/calculate/Algorithim/Select.cpp.o" "gcc" "CMakeFiles/a.out.dir/calculate/Algorithim/Select.cpp.o.d"
+  "/home/omer/Desktop/projects in linux/advenced programming/Advanced-Programming-Project/calculate/Distance/Canberra.cpp" "CMakeFiles/a.out.dir/calculate/Distance/Canberra.cpp.o" "gcc" "CMakeFiles/a.out.dir/calculate/Distance/Canberra.cpp.o.d"
+  "/home/omer/Desktop/projects in linux/advenced programming/Advanced-Programming-Project/calculate/Distance/Chebyshev.cpp" "CMakeFiles/a.out.dir/calculate/Distance/Chebyshev.cpp.o" "gcc" "CMakeFiles/a.out.dir/calculate/Distance/Chebyshev.cpp.o.d"
+  "/home/omer/Desktop/projects in linux/advenced programming/Advanced-Programming-Project/calculate/Distance/Distance.cpp" "CMakeFiles/a.out.dir/calculate/Distance/Distance.cpp.o" "gcc" "CMakeFiles/a.out.dir/calculate/Distance/Distance.cpp.o.d"
+  "/home/omer/Desktop/projects in linux/advenced programming/Advanced-Programming-Project/calculate/Distance/Euclidean.cpp" "CMakeFiles/a.out.dir/calculate/Distance/Euclidean.cpp.o" "gcc" "CMakeFiles/a.out.dir/calculate/Distance/Euclidean.cpp.o.d"
+  "/home/omer/Desktop/projects in linux/advenced programming/Advanced-Programming-Project/calculate/Distance/Manhattan.cpp" "CMakeFiles/a.out.dir/calculate/Distance/Manhattan.cpp.o" "gcc" "CMakeFiles/a.out.dir/calculate/Distance/Manhattan.cpp.o.d"
+  "/home/omer/Desktop/projects in linux/advenced programming/Advanced-Programming-Project/calculate/Distance/Minkowski.cpp" "CMakeFiles/a.out.dir/calculate/Distance/Minkowski.cpp.o" "gcc" "CMakeFiles/a.out.dir/calculate/Distance/Minkowski.cpp.o.d"
+  "/home/omer/Desktop/projects in linux/advenced programming/Advanced-Programming-Project/calculate/ReadCSV.cpp" "CMakeFiles/a.out.dir/calculate/ReadCSV.cpp.o" "gcc" "CMakeFiles/a.out.dir/calculate/ReadCSV.cpp.o.d"
+  "/home/omer/Desktop/projects in linux/advenced programming/Advanced-Programming-Project/main.cpp" "CMakeFiles/a.out.dir/main.cpp.o" "gcc" "CMakeFiles/a.out.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build-for-linux/CMakeFiles/a.out.dir/cmake_clean.cmake
@@ -0,0 +1,29 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/a.out.dir/calculate/Algorithim/Knn.cpp.o"
+  "CMakeFiles/a.out.dir/calculate/Algorithim/Knn.cpp.o.d"
+  "CMakeFiles/a.out.dir/calculate/Algorithim/Select.cpp.o"
+  "CMakeFiles/a.out.dir/calculate/Algorithim/Select.cpp.o.d"
+  "CMakeFiles/a.out.dir/calculate/Distance/Canberra.cpp.o"
+  "CMakeFiles/a.out.dir/calculate/Distance/Canberra.cpp.o.d"
+  "CMakeFiles/a.out.dir/calculate/Distance/Chebyshev.cpp.o"
+  "CMakeFiles/a.out.dir/calculate/Distance/Chebyshev.cpp.o.d"
+  "CMakeFiles/a.out.dir/calculate/Distance/Distance.cpp.o"
+  "CMakeFiles/a.out.dir/calculate/Distance/Distance.cpp.o.d"
+  "CMakeFiles/a.out.dir/calculate/Distance/Euclidean.cpp.o"
+  "CMakeFiles/a.out.dir/calculate/Distance/Euclidean.cpp.o.d"
+  "CMakeFiles/a.out.dir/calculate/Distance/Manhattan.cpp.o"
+  "CMakeFiles/a.out.dir/calculate/Distance/Manhattan.cpp.o.d"
+  "CMakeFiles/a.out.dir/calculate/Distance/Minkowski.cpp.o"
+  "CMakeFiles/a.out.dir/calculate/Distance/Minkowski.cpp.o.d"
+  "CMakeFiles/a.out.dir/calculate/ReadCSV.cpp.o"
+  "CMakeFiles/a.out.dir/calculate/ReadCSV.cpp.o.d"
+  "CMakeFiles/a.out.dir/main.cpp.o"
+  "CMakeFiles/a.out.dir/main.cpp.o.d"
+  "a.out"
+  "a.out.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/a.out.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
