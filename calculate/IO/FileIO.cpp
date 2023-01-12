447edf2fbd58c03e12d0e84890599c23e805df03
omer <omerbar450@gmail.com>
2023-01-12 20:53:42 +0200

    adding the fileIO

---
+++ calculate/IO/FileIO.cpp
@@ -0,0 +1,5 @@
+//
+// Created by omer on 1/12/23.
+//
+
+#include "FileIO.h"
---
+++ calculate/IO/FileIO.h
@@ -0,0 +1,14 @@
+//
+// Created by omer on 1/12/23.
+//
+
+#ifndef A_OUT_FILEIO_H
+#define A_OUT_FILEIO_H
+
+
+class FileIO {
+
+};
+
+
+#endif //A_OUT_FILEIO_H
